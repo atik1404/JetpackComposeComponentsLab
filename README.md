8a0d75bad832fd3af1c23b2712f87790726b5b31
Atik Faysal <atikfaysal1404@gmail.com>
2023-07-05 09:08:54 +0600

    add topBar

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # JetpackComposeLab
+
+- Make a complex design
+- Handle click listener
+- Select list item
+- Add a bottom bar and drawer
+- Api call and display data 
+- Navigate to another screen using nav graph
+- implement multi module and clean architecture 
--- app/src/main/java/com/lab/compose/MainActivity.kt
+++ app/src/main/java/com/lab/compose/MainActivity.kt
@@ -3,13 +3,23 @@ package com.lab.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lab.compose.ui.theme.JetpackComposeLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +28,39 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeLabTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    UserCardItem()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UserCardItem(){
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Compose Lab")
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary)
+            )
+        }
+    ) {
+        val modifier = Modifier.padding(it)
+        Text(
+            text = "Hello Compose!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeLabTheme {
-        Greeting("Android")
+        UserCardItem()
     }
 }
