87cba6d401b676a7a140a3369ce0a8d63bb4b709
Atik Faysal <atikfaysal1404@gmail.com>
2023-07-06 08:41:47 +0600

    working on user item design

--- app/src/main/java/com/lab/compose/MainActivity.kt
+++ app/src/main/java/com/lab/compose/MainActivity.kt
@@ -3,23 +3,49 @@ package com.lab.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraintsScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.UiComposable
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.lab.compose.ui.theme.JetpackComposeLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,11 +76,85 @@ fun UserCardItem(){
         }
     ) {
         val modifier = Modifier.padding(it)
-        Text(
-            text = "Hello Compose!",
-            modifier = modifier
-        )
+        Column(modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.LightGray)
+        ){
+            ConstraintLayout(){
+                val (userItem, addButton) = createRefs()
+                Box(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .background(shape = RoundedCornerShape(10.dp), color = Color.White)
+                        .constrainAs(userItem) {
+                            top.linkTo(parent.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom)
+                        }
+                ){
+                    Column {
+                        Spacer(modifier = Modifier.height(40.dp))
+                        Image(
+                            painter = painterResource(id = R.drawable.avatar),
+                            contentDescription = "",
+                            contentScale = ContentScale.Fit,
+                            modifier = Modifier
+                                .clip(shape = CircleShape)
+                                .size(50.dp)
+                                .align(Alignment.CenterHorizontally)
+                                .border(1.dp, Color.Gray, CircleShape),
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(
+                            text = "Atik faysal",
+                            color = Color.Black,
+                            fontSize = 16.sp,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .align(alignment = Alignment.CenterHorizontally)
+                                .padding(6.dp, 5.dp))
+                        Text(
+                            text = "Software Engineer",
+                            textAlign = TextAlign.Center,
+                            color = Color.Gray,
+                            fontSize = 14.sp,
+                            modifier = Modifier
+                                .align(alignment = Alignment.CenterHorizontally)
+                                .padding(6.dp, 5.dp))
+                        Spacer(modifier = Modifier.height(15.dp))
+                    }
+                }
+
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(30.dp)
+                        .constrainAs(addButton) {
+                            start.linkTo(userItem.start)
+                            end.linkTo(userItem.end)
+                            bottom.linkTo(userItem.bottom)
+                        }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_add),
+                        contentDescription = "",
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun displayUser(users : List<User>){
+//    LazyColumn(modifier = Modifier) {
+//        items(users.size) { data ->
+//            UserCardItem(users = data)
+//        }
+//    }
 }
 
 @Preview(showBackground = true)
@@ -64,3 +164,17 @@ fun GreetingPreview() {
         UserCardItem()
     }
 }
+
+fun getUsers() : List<User>{
+    return listOf(
+        User("Atik Faysal", "Software Eng", 1),
+        User("Tom cruis", "Actor", 2),
+        User("Abc xyz", "Actor", 3),
+    )
+}
+
+data class User(
+    val name : String,
+    val designation : String,
+    val id : Int
+)
--- app/src/main/java/com/lab/compose/ui/theme/Color.kt
+++ app/src/main/java/com/lab/compose/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Black = Color(0xFF000000)
+val Black80 = Color(0xFF252525)
+val Black50 = Color(0xFF191919)
+val Red = Color(0xFFB3261E)
+val LightGray = Color(0xffF6F6F6)
