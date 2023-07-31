ec4d091007b8d5bef17d5d4470b42e2d5edb3e66
Atik <atikfaysal.jatri@gmail.com>
2023-07-31 17:46:20 +0600

    working on login page design

--- app/src/main/java/com/lab/compose/LoginActivity.kt
+++ app/src/main/java/com/lab/compose/LoginActivity.kt
@@ -12,9 +12,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,13 +28,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.lab.compose.ui.theme.JetpackComposeLabTheme
 
 class LoginActivity : ComponentActivity() {
@@ -51,39 +58,51 @@ class LoginActivity : ComponentActivity() {
 @Preview
 @Composable
 private fun LoginUi(){
-    Column(
-        modifier = Modifier.padding(15.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    ConstraintLayout(
+        modifier = Modifier.fillMaxSize()
     ) {
-        val userName = remember { mutableStateOf(TextFieldValue()) }
-        val password = remember { mutableStateOf(TextFieldValue()) }
-        Text(text = "Login")
+        val (userData) = createRefs()
+        Column(
+            modifier = Modifier.padding(15.dp)
+                .constrainAs(userData){
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        ) {
+            val userName = remember { mutableStateOf(TextFieldValue()) }
+            val password = remember { mutableStateOf(TextFieldValue()) }
+            Text(text = "Login")
 
-        Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(10.dp))
 
-        TextField(
-            value = userName.value,
-            placeholder = { Text(text = "Username")},
-            onValueChange = {}
-        )
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        TextField(
-            value = password.value,
-            onValueChange = {},
-            placeholder = { Text(text = "Password")}
-        )
-
-        ClickableText(
-            text = AnnotatedString("Sign up here"),
-            onClick = {},
-            modifier = Modifier
-                .padding(15.dp),
-            style = TextStyle(
-                fontSize = 15.sp,
-                color = Color.Black
+            TextField(
+                value = userName.value,
+                placeholder = { Text(text = "Username")},
+                onValueChange = {}
             )
-        )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            TextField(
+                value = password.value,
+                onValueChange = {},
+                placeholder = { Text(text = "Password")}
+            )
+
+            ClickableText(
+                text = AnnotatedString("Sign up here"),
+                onClick = {},
+                modifier = Modifier
+                    .padding(15.dp),
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    color = Color.Black
+                )
+            )
+        }
     }
+
+
 }
